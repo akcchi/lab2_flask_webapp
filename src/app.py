1f9f638b2de347255297bdf480e953c9fcb0c57e
Andy Chi <andy.chi24@imperial.ac.uk>
2024-10-21 16:34:02 +0100

    Update query handling

--- src/app.py
+++ src/app.py
@@ -33,7 +33,7 @@ def submit():
     return render_template(show, name=input_name, age=input_age, pet=pref)
 
 
-@app.route("/query")
+@app.route("/query", methods=["GET"])
 def process_query():
     query = request.args.get['q']
     if query == "dinosaurs":
@@ -42,4 +42,4 @@ def process_query():
         ans = "Unknown"
     else:
         ans = "No query provided"
-    return ans
+    return f'{ans}'
