f62d5e9f89b97e43759c98afff643c814cb53ca7
Andy Chi <andy.chi24@imperial.ac.uk>
2024-11-04 15:07:30 +0000

    Update list handling

--- src/app.py
+++ src/app.py
@@ -58,15 +58,18 @@ def hello_user():
 
 @app.route("/submit_user", methods=["POST"])
 def submit_user():
-    repo_list = []
+    # repo_list = []
+    repo_list = "<ul>"
     in_name = request.form.get("user")
     response = requests.get(f"https://api.github.com/users/{in_name}/repos")
     if response.status_code == 200:
         repos = response.json()  # Returns list of repo entities
         for repo in repos:
             temp_name = repo["full_name"]
-            temp_append = f"{temp_name}"
-            repo_list.append(temp_append)
+            temp_append = f"<li> {temp_name} </li>"
+            # repo_list.append(temp_append)
+            repo_list += temp_append
+        repo_list += "</ul>"
 
     return render_template(
         "result_user.html", user=in_name, repo_list=repo_list)
